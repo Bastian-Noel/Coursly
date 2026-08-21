0eae43dfae043aa61bd0b59ac9071c156a04339c
Bastian-Noel <71983581+Bastian-Noel@users.noreply.github.com>
2026-08-21 18:38:24 +0200

    fix: supprimer le doublon des types de cours dynamiques

--- Coursly/App/CalendarStore+CourseTypes.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension CalendarStore {
-    var observedCourseTypeLabels: [String] {
-        Array(Set(events.compactMap(\.displayTypeLabel)))
-            .filter { !$0.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty }
-            .sorted { $0.localizedCaseInsensitiveCompare($1) == .orderedAscending }
-    }
-}
